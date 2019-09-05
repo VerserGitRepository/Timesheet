357fc41fbd60c57f057f679e68a5123ff513b71c
Verser <verser@outlook.com.au>
2019-09-05 17:08:25 +1000

    Roles Based Access

---
+++ TimeSheet/TimeSheet/Models/ListItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeSheet.Models
+{
+    public class ListItems
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
--- TimeSheet/TimeSheet/ServiceHelper/LoginService.cs
+++ TimeSheet/TimeSheet/ServiceHelper/LoginService.cs
@@ -28,5 +28,29 @@ namespace TimeSheet.ServiceHelper
             }
             return returnmessage;
         }
+        public async static Task<List<ListItems>> UserRoleList(string UserName)
+        {
+           var returnmessage = new List<ListItems>();
+            string BaseUri = ConfigurationManager.AppSettings["LoginManagerBase"];
+
+            using (HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                HttpResponseMessage response = client.GetAsync(string.Format("Login/{0}/UserRole",UserName)).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<ListItems>>();
+                    foreach (var p in result)
+                    {
+                        returnmessage.Add(new ListItems() { Id = p.Id, Value = p.Value });
+                    }
+
+                }
+            }
+            return returnmessage;
+        }
     }
 }
+
+                   
