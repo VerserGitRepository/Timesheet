8efe6085cb9edc0ef070108fbb7898563e535128
Verser <verser@outlook.com.au>
2019-09-10 12:50:06 +1000

    New Changes Basan on completed booking

--- TimeSheet/TimeSheet/Controllers/CompletedBookingController.cs
+++ TimeSheet/TimeSheet/Controllers/CompletedBookingController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using TimeSheet.Models;
 using TimeSheet.ServiceHelper;
 
@@ -23,6 +23,55 @@ namespace TimeSheet.Controllers
                 model.CompletedTimeSheetList = TimeSheetAPIHelperService.TimeSheetCompletedList().Result;
                 return View(model);
             }
+        }
+        [HttpPost]
+        public ActionResult ExportCompletedScheduls()
+        {
+            var TimeSheetExportData = new List<CompletedtimesheetExportModel>();
+
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else
+            {
+              var  TimeSheetmodel = TimeSheetAPIHelperService.TimeSheetCompletedList().Result;
+                foreach (var item in TimeSheetmodel)
+                {                   
+
+                    TimeSheetExportData.Add(new CompletedtimesheetExportModel
+                    {
+                        ProjectName = item.ProjectName,
+                        CandidateName = item.CandidateName,
+                        OpportunityNumber = item.OpportunityNumber,
+                        Activity = item.Activity,
+                        WarehouseName = item.WarehouseName,
+                        StartTime =  item.StartTime,
+                        EndTime = item.EndDate,
+                        JobNo = item.JobNo,
+                        OLATarget = item.OLATarget,
+                        ActualQuantity = item.ActualQuantity,
+                        Day = item.Day.Value.Date,
+                        Status = item.Status,
+                        TimeSheetComments = item.TimeSheetComments
+                    });
+                }
+                GridView gv = new GridView();          
+                gv.DataSource = TimeSheetExportData;
+                gv.DataBind();
+                Response.ClearContent();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition", "attachment; filename=CompletedTimeScheduls.xls");
+                Response.ContentType = "application/ms-excel";
+                Response.Charset = "";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+                gv.RenderControl(htw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+            return RedirectToAction("Index", "CompletedBooking");
         }
     }
 }
---
+++ TimeSheet/TimeSheet/Models/CompletedtimesheetExportModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TimeSheet.Models
+{
+    public class CompletedtimesheetExportModel
+    {
+        public string ProjectName { get; set; }
+        public string CandidateName { get; set; }
+        public int OpportunityNumber { get; set; }
+        public string Activity { get; set; }
+        public string WarehouseName { get; set; }
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime? StartTime { get; set; }
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime? EndTime { get; set; }
+        public string JobNo { get; set; }     
+        public int? OLATarget { get; set; }
+        public int? ActualQuantity { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? Day { get; set; }       
+        public string Status { get; set; }
+        public string TimeSheetComments { get; set; }
+
+    }
+}
