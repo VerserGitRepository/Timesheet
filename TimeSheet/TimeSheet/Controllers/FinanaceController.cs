d5633e443261d70780c3287edd881ad8e71bec05
Ragini <ragini.malhotra@verser.com.au>
2019-09-20 12:52:35 +1000

    Weekly Report changes

--- TimeSheet/TimeSheet/Controllers/FinanaceController.cs
+++ TimeSheet/TimeSheet/Controllers/FinanaceController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TimeSheet.Models;
 using TimeSheet.ServiceHelper;
 
 namespace TimeSheet.Controllers
@@ -64,5 +68,66 @@ namespace TimeSheet.Controllers
             }
 
         }
+
+        [HttpGet]
+        public ActionResult ApprovedResourceDetails(string CandidateName)
+        {
+            CompletedTimesheetModel model = new CompletedTimesheetModel();
+            var AlltimesheetRecords = TimeSheetAPIHelperService.TimeSheetCompletedList().Result;
+            model.CompletedTimeSheetList = AlltimesheetRecords.Where(c => c.CandidateName == CandidateName).ToList();
+            model.StatusList = new SelectList(ListItemService.StatusList().Result, "ID", "Value");
+            return PartialView("ApprovedResourceDetails", model);
+        }
+
+        [HttpPost]
+        public ActionResult ExportCompletedScheduls()
+        {
+            var TimeSheetExportData = new List<CompletedtimesheetExportModel>();
+
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else
+            {
+                var TimeSheetmodel = TimeSheetAPIHelperService.TimeSheetApprovedList().Result;
+                foreach (var item in TimeSheetmodel)
+                {
+
+                    TimeSheetExportData.Add(new CompletedtimesheetExportModel
+                    {
+                        ProjectName = item.ProjectName,
+                        CandidateName = item.CandidateName,
+                        OpportunityNumber = item.OpportunityNumber,
+                        Activity = item.Activity,
+                        WarehouseName = item.WarehouseName,
+                        StartTime = item.StartTime,
+                        EndTime = item.EndTime,
+                        JobNo = item.JobNo,
+                        OLATarget = item.OLATarget,
+                        ActualQuantity = item.ActualQuantity,
+                        Day = item.Day.Value.Date,
+                        Status = item.Status,
+                        TimeSheetComments = item.TimeSheetComments
+                    });
+                }
+                GridView gv = new GridView();
+                gv.DataSource = TimeSheetExportData;
+                gv.DataBind();
+                Response.ClearContent();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition", "attachment; filename=ApprovedTimeSchedule.xls");
+                Response.ContentType = "application/ms-excel";
+                Response.Charset = "";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+                gv.RenderControl(htw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+            return RedirectToAction("WeeklyReport", "Finanace");
+        }
+
     }
 }
