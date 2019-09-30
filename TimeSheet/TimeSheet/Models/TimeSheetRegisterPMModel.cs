1644368eac77d27cfd6b40c8fd8c7030936da8a0
Kalyan Vedula <kalyan.vedula@verser.com.au>
2019-09-30 11:07:49 +1000

    changes PM Booking

---
+++ TimeSheet/TimeSheet/Models/TimeSheetRegisterPMModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeSheet.Models
+{
+    public class TimeSheetRegisterPMModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "StartTime Is Mandatory")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime? StartTime { get; set; }
+        [Required(ErrorMessage = "EndTime Is Mandatory")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime? EndTime { get; set; }
+        [Required(ErrorMessage = "Day Is Mandatory")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? Day { get; set; }
+        
+        public int? OLATarget { get; set; }
+        public int? ActualQuantity { get; set; }
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string TimeSheetComments { get; set; }
+        [Required(ErrorMessage = "Opportunity Is Mandatory")]
+        public int? OpportunityNumberID { get; set; }
+        [Required(ErrorMessage = "Warehouse Is Mandatory")]
+        public int? WarehouseNameId { get; set; }
+        [Required(ErrorMessage = "Activity Is Mandatory")]
+        public int? ServiceActivityId { get; set; }
+        public string Colour { get; set; }
+        [Required(ErrorMessage = "Resource Is Mandatory")]
+        public int? CandidateNameId { get; set; }
+        public string CandidateName { get; set; }
+        public int? StatusID { get; set; }
+        [Required(ErrorMessage = "Project Is Mandatory")]
+        public int? ProjectID { get; set; }
+        public string JobNo { get; set; }
+        [Required(ErrorMessage = "Job Is Mandatory")]
+        public int? JobID { get; set; }
+        public int? EmploymentTypeID { get; set; }
+        [Required(ErrorMessage = "Resource selection is Mandatory")]
+        public List<int?> ResourceIDs { get; set; }
+        public string FullName { get; set; }
+        public string Vechicles { get; set; }
+    }
+}
