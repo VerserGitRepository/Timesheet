f4d0ad577aa7bfa3231d07431b17f973e60ff04e
Kalyan Vedula <kalyan.vedula@verser.com.au>
2019-10-01 17:22:10 +1000

    projection, PM booking and vehicle timeline changes

--- TimeSheet/TimeSheet/Models/UpdateProjectManagerModel.cs
+++ TimeSheet/TimeSheet/Models/UpdateProjectManagerModel.cs
@@ -7,15 +7,18 @@ namespace TimeSheet.Models
 {
     public class UpdateProjectManagerModel
     {
-        public int Id { get; set; }
+        public int OpportunityNumberID { get; set; }
+        public int ProjectID { get; set; }
+        public int WareHouseID { get; set; }
+        public int ResourceID { get; set; }
+        public int ServiceActivityID { get; set; }        
+        public string Colour { get; set; }
         public int? StatusID { get; set; }
-        public int? ActualQuantity { get; set; }
         public string TimeSheetComments { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
         public DateTime? Day { get; set; }
         public DateTime? EndDate { get; set; }
-        public string FullName { get; set; }
-
+        
     }
 }
