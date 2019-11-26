da25825351685baabc4aa983e96d8ca3803fb38f
Kalyan Vedula <kalyan.vedula@verser.com.au>
2019-11-26 13:48:15 +1100

    missed commit

--- TimeSheet/TimeSheet/Models/ProjectionUpdate.cs
+++ TimeSheet/TimeSheet/Models/ProjectionUpdate.cs
@@ -8,7 +8,7 @@ namespace TimeSheet.Models
     public class ProjectionUpdate
     {
         public int Id { get; set; }       
-        public int Quantity { get; set; }
+        public decimal Quantity { get; set; }
         public bool? IsActive { get; set; }
         public string Comments { get; set; }
         public string DateInvoiced { get; set; }
