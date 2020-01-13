3db1e23053d226913f776de38c7a1b639e60272b
Verser <verser@outlook.com.au>
2020-01-13 12:18:54 +1100

    Latest Update To Master

--- TimeSheet/TimeSheet/ServiceHelper/TimeSheetAPIHelperService.cs
+++ TimeSheet/TimeSheet/ServiceHelper/TimeSheetAPIHelperService.cs
@@ -1,8 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
