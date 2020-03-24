9a1da6561267464dd1772468c25793d4b9e07e5f
Verser <verser@outlook.com.au>
2020-03-25 10:04:54 +1100

    User Access Check

--- TimeSheet/TimeSheet/ServiceHelper/UserRoles.cs
+++ TimeSheet/TimeSheet/ServiceHelper/UserRoles.cs
@@ -1,125 +1,89 @@
-﻿using System.Web;
-using System.Web.Http.Filters;
-using System.Web.Mvc;
-using System.Web.Routing;
-using TimeSheet.Controllers;
+﻿using System.Linq;
+using System.Web;
 
 namespace TimeSheet.ServiceHelper
 {
     public class UserRoles
     {
+
+        public static bool UserEditCompleteBookings()
+        {
+            string UserName = HttpContext.Current.Session["Username"] != null ? HttpContext.Current.Session["Username"].ToString() : null;
+            if (string.IsNullOrEmpty(UserName))
+            {
+                return false;
+            }
+
+            var _roles = LoginService.UserRoleList(UserName).Result;
+            return _roles.Where(r => r.Value == "Administrator"
+            || r.Value == "Accounts" || r.Value == "WarehouseManager" || r.Value == "ProjectmanagerAdmin").FirstOrDefault() != null ? true : false;
+        }
+        public static bool PMEditCompleteBookings()
+        {
+            string UserName = HttpContext.Current.Session["Username"] != null ? HttpContext.Current.Session["Username"].ToString() : null;
+            if (string.IsNullOrEmpty(UserName))         
+              return false;          
+            var _roles = LoginService.UserRoleList(UserName).Result;
+            return _roles.Where(r => r.Value == "Administrator" || r.Value == "Accounts" ||  r.Value == "ProjectmanagerAdmin")
+            .FirstOrDefault() != null ? true : false;
+        }
+
         public static bool UserCanEditTimesheet()
         {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if (HttpContext.Current.Session["Accounts"] != null ||  HttpContext.Current.Session["WarehouseManager"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return UserEditCompleteBookings();
         }
         public static bool UserCanRateTimesheet()
-        {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if (HttpContext.Current.Session["Accounts"] != null ||  HttpContext.Current.Session["WarehouseManager"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-            }
-            return false;
+        {          
+            return UserEditCompleteBookings();
         }
         public static bool SiteAdmin()
         {
-            if (HttpContext.Current.Session["Username"] != null)
+            string UserName = HttpContext.Current.Session["Username"] != null ? HttpContext.Current.Session["Username"].ToString() : null;
+            if (string.IsNullOrEmpty(UserName))
             {
-                if (HttpContext.Current.Session["Accounts"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
+                return false;
             }
-            return false;
+            var _roles = LoginService.UserRoleList(UserName).Result;
+            return _roles.Where(r => r.Value == "Administrator" || r.Value == "Accounts").FirstOrDefault() != null ? true : false;
         }
         public static bool UserCanEditCompletedTimesheet()
         {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if (HttpContext.Current.Session["WarehouseManager"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return UserEditCompleteBookings();
         }
 
         public static bool UserCanRegisterPMTimesheet()
-        {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if ( HttpContext.Current.Session["Accounts"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-
-            }
-            return false;
+        {           
+            return PMEditCompleteBookings();
         }
-
         public static bool UserCanRegisterTimesheet()
-        {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                return true;
-               
-            }
-            return false;
+        {           
+            return UserEditCompleteBookings();
         }
         public static bool UserCanViewResourceDetails()
         {
             if (HttpContext.Current.Session["Username"] != null)
             {
-                if (HttpContext.Current.Session["WarehouseManager"]   != null || HttpContext.Current.Session["Accounts"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-
+                return true;     
             }
             return false;
         }
         public static bool UserCanApproveTimesheet()
-        {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if (HttpContext.Current.Session["WarehouseManager"] != null || HttpContext.Current.Session["Accounts"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-            }
-            return false;
+        {           
+            return UserEditCompleteBookings();
         }
         public static bool HRUser()
         {
-            if (HttpContext.Current.Session["Username"] != null)
+            string UserName = HttpContext.Current.Session["Username"] != null ? HttpContext.Current.Session["Username"].ToString() : null;
+            if (string.IsNullOrEmpty(UserName))
             {
-                if (HttpContext.Current.Session["HR"] != null && HttpContext.Current.Session["HR"].ToString().ToLower()=="true")
-                {                    
-                   return true;                   
-                }
+                return false;
             }
-            return false;
+            var _roles = LoginService.UserRoleList(UserName).Result;
+            return _roles.Where(r => r.Value == "HRAdmin").FirstOrDefault() != null ? true : false;
         }
         public static bool UserCanAddOrUpdateProjection()
-        {
-            if (HttpContext.Current.Session["Username"] != null)
-            {
-                if (HttpContext.Current.Session["Accounts"] != null || HttpContext.Current.Session["ProjectManager"] != null || HttpContext.Current.Session["Administrator"] != null)
-                {
-                    return true;
-                }
-
-            }
-            return false;
+        {          
+            return PMEditCompleteBookings();
         }
     }
 }
