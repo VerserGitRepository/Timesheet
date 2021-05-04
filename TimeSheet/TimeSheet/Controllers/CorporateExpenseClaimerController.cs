5739ff40757016e1fb8bdcb586d526577f6222b8
VerserGitRepository <verser@outlook.com.au>
2021-05-04 10:07:37 +1000

    new changes

---
+++ TimeSheet/TimeSheet/Controllers/CorporateExpenseClaimerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeSheet.Controllers
+{
+    public class CorporateExpenseClaimerController : Controller
+    {
+        // GET: CorporateExpenseClaimer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TimeSheet/TimeSheet/Controllers/PersonalExpenseClaimerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeSheet.Controllers
+{
+    public class PersonalExpenseClaimerController : Controller
+    {
+        // GET: PersonalExpenseClaimer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
