222b05de10720c3446c0a98d31b2dd16ad099aee
VerserGitRepository <verser@outlook.com.au>
2021-05-28 10:04:58 +1000

    Update AdminModel.cs

--- TimeSheet/TimeSheet/Models/AdminModel.cs
+++ TimeSheet/TimeSheet/Models/AdminModel.cs
@@ -8,6 +8,12 @@ namespace TimeSheet.Models
 {
     public class AdminModel
     {
+        public AdminModel()
+        {
+            InvoiceSupplierlist = new List<InvoiceSuppliersViewModel>();
+            SuppliersGLCodelist = new List<SuppliersGLCodeViewModel>();
+            CorporateCardHolderlist = new List<CorporateCardHolderViewModel>();
+        }
         public List<OpportunityListModel> OpportunityList { get; set; }
         public List<ProjectListModel> ProjectDetailsList { get; set; }
         public List<ResourceListModel> ResourceList { get; set; }
@@ -15,5 +21,8 @@ namespace TimeSheet.Models
         public List<ListItemViewModel> ProjectManagerList { get; set; }
         public List<ListItemViewModel> SalesManagerList { get; set; }
         public List<OpportunityListItem> OpportunitesList { get; set; }
+        public List<InvoiceSuppliersViewModel> InvoiceSupplierlist{ get; set; }
+        public List<SuppliersGLCodeViewModel> SuppliersGLCodelist { get; set; }
+        public List<CorporateCardHolderViewModel> CorporateCardHolderlist { get; set; }
     }
 }
